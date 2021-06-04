12f85d0e4f228bac2a5b14ba6f3523ce7954004c
Shiragu <pipboy2009@yandex.ru>
2021-06-04 09:58:42 +0300

    first upload

---
+++ src/components/favorite.component.js
@@ -0,0 +1,53 @@
+import { Component } from "../core/component";
+import { apiService } from "../services/api.service";
+import { renderPost } from "../templates/post.template";
+
+export class FavoriteComponent extends Component {
+  constructor(id, { loader }) {
+    super(id);
+    this.loader = loader;
+  }
+
+  init() {
+    this.$el.addEventListener("click", linkClickHandler.bind(this));
+  }
+
+  onShow() {
+    const favorites = JSON.parse(localStorage.getItem("favorites"));
+    const html = renderList(favorites);
+    this.$el.insertAdjacentHTML("afterbegin", html);
+  }
+
+  onHide() {
+    this.$el.innerHTML = "";
+  }
+}
+
+async function linkClickHandler(event) {
+  event.preventDefault();
+  if (event.target.classList.contains("js-link")) {
+    const postID = event.target.dataset.id;
+    this.$el.innerHTML = "";
+    this.loader.show();
+    const post = await apiService.fetchPostByID(postID);
+    this.loader.hide();
+    this.$el.insertAdjacentHTML(
+      "afterbegin",
+      renderPost(post, { withButton: false })
+    );
+  }
+}
+
+function renderList(list = []) {
+  if (list && list.length) {
+    return `<ul>
+      ${list
+        .map(
+          (i) =>
+            `<li><a href="#" class="js-link" data-id="${i.id}">${i.title}</a></li>`
+        )
+        .join(" ")}
+    </ul>`;
+  }
+  return `<p class="center">В избранном пока ничего нет</p>`;
+}
---
+++ src/components/guide.component.js
@@ -0,0 +1,7 @@
+import { Component } from "../core/component";
+
+export class GuideComponent extends Component {
+  constructor(id) {
+    super(id);
+  }
+}
